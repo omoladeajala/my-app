c5a9979530205fcc19eabda818fc172113ba2b07
Omolade Ajala <omoladeajala2021@gmail.com>
2025-08-27 23:06:39 +0100

    Remove unused files and components to streamline the project structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import {useState} from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ export default function interview() {
+  return (
+    <div className="interview">
+      <JavaScript questions />
+    </div>
+  );
+ }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ const question = [ {
+  No: "1",
+  question: "What is a variables?",
+  answer: "Variables is a named storage location in memory that can hold a value.",
+ },
+{
+  No: "2",
+  question: "What is a constant?",
+  answer: "Constant is a named data item whose value remains fixed throughout the program's execution.",
+},
+{
+  No: "3",
+  question: "What is an Array used for?",
+  answer: "Array is used to store collection of elements of same data type.",
+},
+{
+  No: "4",
+  question: "Examples of an array?",
+  answer: 'String (Both single and double stings) e.g... ["Red", "Blue", "Bolu"] ',
+},
+{
+  No: "5",
+  question: "What is a tuple?",
+  answer: "It is a type of array with a pre-defined length ", 
+},
+{
+  No: "6",
+  question: "Examples of a tuple?",
+  answer: 'Strings and Numbers e.g... ["Alice", 18, "Benson", 34]', 
+},
+{
+  No: "7",
+  question: "",
+  answer: "", 
+},
+{
+  No: "8",
+  question: "M",
+  answer: "", 
+},
+{
+  No: "9",
+  question: "M",
+  answer: "", 
+},
+{
+  No: "10",
+  question: "",
+  answer: "", 
+},
+{
+  No: "11",
+  question: "",
+  answer: "", 
+},
+{
+  No: "12",
+  question: "",
+  answer: "", 
+},
+{
+  No: "13",
+  question: "",
+  answer: "", 
+},
+{
+  No: "14",
+  question: "",
+  answer: "", 
+},
+{
+  No: "15",
+  question: "",
+  answer: "", 
+},
+{
+  No: "16",
+  question: "",
+  answer: "", 
+},
+{
+  No: "17",
+  question: "",
+  answer: "", 
+},
+{
+  No: "18",
+  question: "",
+  answer: "", 
+},
+{
+  No: "19",
+  question: "",
+  answer: "", 
+},
+{
+  No: "20",
+  question: "",
+  answer: "", 
+},
+]
+
+function JavaScript() {
+
+return (
+  <div className="">
+
+  </div>
+  )
+}
